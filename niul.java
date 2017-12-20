2469f092ff558edb1b37208b0fa9ada77c95f17f
NIUL <niul@ubuntu.ubuntu-domain>
2017-12-20 09:47:12 +0530

    initial java file

---
+++ niul.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class niul
+{
+
+public static int fac(int n)
+{
+ if(n==0)
+  {
+    return 1;
+  }
+ else
+  {
+    return n*fac(n-1);
+  }
+}
+
+public static int add(int a,int b)
+{
+return (a+b);
+}
+
+public static void main(String[] args)
+ { 
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Hello world!!");
+     System.out.println("enter the number:");
+     int n = sc.nextInt();
+     System.out.println(fac(n));
+     System.out.println("enter the two numbers:");
+     int a = sc.nextInt();
+     int b = sc.nextInt();
+     System.out.println(add(a,b));
+ }
+}
